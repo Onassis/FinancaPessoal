85852ac46005a2601cf3f08f51755bf49372972d
Onassis <onassis.tavares@gmail.com>
2024-08-06 17:15:39 -0300

    Fragmento SelectHtml

---
+++ src/main/java/br/com/fenix/dominio/dto/Option.java
@@ -0,0 +1,19 @@
+package br.com.fenix.dominio.dto;
+
+public  class Option {
+    private String value;
+    private String text;
+
+    public Option(String value, String text) {
+        this.value = value;
+        this.text = text;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
