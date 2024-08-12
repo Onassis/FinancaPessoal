e87fb05ba7757693dd6b92510139fb68c116be1c
“onassis” <onassis.tavares@gmail.com>
2024-08-12 19:41:17 -0300

    alteracao casa

---
+++ src/main/java/br/com/fenix/fi/conta/MainController.java
@@ -0,0 +1,51 @@
+package br.com.fenix.fi.conta;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import br.com.fenix.dominio.dto.Option;
+import ch.qos.logback.core.model.Model;
+
+@Controller("/Select")
+public class MainController {
+
+   
+    @ModelAttribute("options")
+    public List<Option> index(Model model) {
+        List<Option> options = Arrays.asList(
+            new Option("1", "Option 1"),
+            new Option("2", "Option 2"),
+            new Option("3", "Option 3")
+        );
+//        model.
+//        .addAttribute("options", options);
+        return options;
+    }
+    
+    @GetMapping
+    public String testeSelect() { 
+    	return "testeSelect"; 
+}
+//
+//    public static class Option {
+//        private String value;
+//        private String text;
+//
+//        public Option(String value, String text) {
+//            this.value = value;
+//            this.text = text;
+//        }
+//
+//        public String getValue() {
+//            return value;
+//        }
+//
+//        public String getText() {
+//            return text;
+//        }
+//    }
+}
